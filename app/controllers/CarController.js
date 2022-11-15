7aea8954179b4236e98c599d2a73646111c2efc7
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2022-11-15 22:38:21 +0700

    add endpoint testing

--- app/controllers/CarController.js
+++ app/controllers/CarController.js
@@ -14,8 +14,8 @@ class CarController extends ApplicationController {
     const offset = this.getOffsetFromRequest(req);
     const limit = req.query.pageSize;
     const query = this.getListQueryFromRequest(req);
-    const cars = await this.carModel.findAll(query);
-    const carCount = await this.carModel.count({ where: query.where, include: query.include, });
+    const cars = this.carModel.findAll(query);
+    const carCount = this.carModel.count({ where: query.where, include: query.include, });
     const pagination = this.buildPaginationObject(req, carCount);
 
     res.status(200).json({
@@ -26,8 +26,8 @@ class CarController extends ApplicationController {
     });
   };
 
-  handleGetCar = async (req, res) => {
-    const car = await this.getCarFromRequest(req);
+  handleGetCar = (req, res) => {
+    const car = this.getCarFromRequest(req);
 
     res.status(200).json(car);
   };
@@ -65,7 +65,7 @@ class CarController extends ApplicationController {
   handleRentCar = async (req, res, next) => {
     try {
       let { rentStartedAt, rentEndedAt } = req.body;
-      const car = await this.getCarFromRequest(req);
+      const car = this.getCarFromRequest(req);
 
       if (!rentEndedAt) rentEndedAt = this.dayjs(rentStartedAt).add(1, "day");
 
